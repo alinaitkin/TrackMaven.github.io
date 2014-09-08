3ec3d59e5768751ffcae536caec258159970aa97
Josh Finnie <josh.finnie@trackmaven.com>
2014-09-08 15:57:51 -0400

    cleaning up config

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,24 +16,12 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
-LINKS = (
-    ('Pelican', 'http://getpelican.com/'),
-    ('Python.org', 'http://python.org/'),
-    ('Jinja2', 'http://jinja.pocoo.org/'),
-    ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (
-    ('You can add links in your config file', '#'),
-    ('Another social link', '#'),)
-
 TWITTER_USERNAME = 'TrackMaven'
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
 
 # Static paths will be copied without parsing their contents
 STATIC_PATHS = ['images', 'extra/CNAME']
@@ -45,6 +33,8 @@ THEME = "theme"
 EXTRA_PATH_METADATA = {
     'extra/CNAME': {'path': 'CNAME'},
 }
+
+IGNORE_FILES = ['node_modules/**/*']
 
 READERS = {'html': None}
 DEBUG = True
