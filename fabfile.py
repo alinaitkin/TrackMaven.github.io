dc45127f9511df46ab52b282726954b8f804328b
Josh Finnie <josh.finnie@trackmaven.com>
2014-09-08 15:55:56 -0400

    cleaning up the fab file a bunch

--- fabfile.py
+++ fabfile.py
@@ -1,19 +1,9 @@
-from fabric.api import hosts, local, env
-import fabric.contrib.project as project
+from fabric.api import local, env
 import os
 
 # Local path configuration (can be absolute or relative to fabfile)
 env.deploy_path = 'output'
 DEPLOY_PATH = env.deploy_path
-
-# Remote server configuration
-production = 'root@localhost:22'
-dest_path = '/var/www'
-
-# Rackspace Cloud Files configuration settings
-env.cloudfiles_username = 'my_rackspace_username'
-env.cloudfiles_api_key = 'my_rackspace_api_key'
-env.cloudfiles_container = 'my_cloudfiles_container'
 
 
 def clean():
@@ -46,32 +36,15 @@ def reserve():
     serve()
 
 
-def preview():
+def prodbuild():
+    clean()
     local('pelican -s publishconf.py')
-
-
-def cf_upload():
-    rebuild()
-    local('cd {deploy_path} && '
-          'swift -v -A https://auth.api.rackspacecloud.com/v1.0 '
-          '-U {cloudfiles_username} '
-          '-K {cloudfiles_api_key} '
-          'upload -c {cloudfiles_container} .'.format(**env))
+    local("echo 'engineroom.trackmaven.com' > {}/CNAME".format(
+        env.deploy_path))
 
 
 def push():
-    rebuild()
+    prodbuild()
     local('git push origin source:source')
     local('ghp-import {}'.format(env.deploy_path))
     local('git push origin gh-pages:master --force')
-
-
-@hosts(production)
-def publish():
-    local('pelican -s publishconf.py')
-    project.rsync_project(
-        remote_dir=dest_path,
-        exclude=".DS_Store",
-        local_dir=DEPLOY_PATH.rstrip('/') + '/',
-        delete=True
-    )
