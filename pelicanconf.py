1748ab12087e3ddf7af74860dc534c2a61470f98
Josh Finnie <josh.finnie@trackmaven.com>
2015-01-08 17:39:10 -0500

    updating some things to work.

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,7 +31,7 @@ DEFAULT_PAGINATION = 10
 ARTICLE_EXCLUDES = (('pages', 'demos'))
 
 # Static paths will be copied without parsing their contents
-STATIC_PATHS = ['images', 'extra/CNAME']
+STATIC_PATHS = ['images', 'extra/CNAME', 'demos']
 
 ARTICLE_URL = 'blog/{slug}/'
 ARTICLE_SAVE_AS = 'blog/{slug}/index.html'
